11dd9a6558c53f6ee240791a5b9b373043693f5f
Vickensi <vickensi.delacruz>
2019-05-15 18:23:04 -0500

    adding-script-header-sticky

--- main.js
+++ main.js
@@ -1,151 +1,231 @@
-let btnSearch = document.getElementById('js-animation-search');
-
-function createElementModal() {
-	let htmltemplate = `<style>
-							body.modal-active {
-								overflow: hidden;
-							}
-							@media (min-width: 1000px) {
-								body.modal-active {
-									padding-right: 16px;
-								}	
-							}
-							#modal-container {
-								position: fixed;
-								height: 100vh;
-								width: 100%;
-								top: 0;
-								left: 0;
-								right: 0;
-								bottom: 0;
-								transform: scale(0);
-								z-index: 1;
-								background: rgba(55, 95, 210, 0.9);
-								overflow-x: hidden;
-				    			overflow-y: auto;
-							}
-							#modal-container .modal-background {
-								text-align: center;
-								height: 100%;
-								width: 100%;
-								display: flex;
-								padding: 15px 0;
-							}
-							#modal-container .modal-background::before {
-								content: "X";
-								position: absolute;
-								top: 1.5rem;
-								right: 1.5rem;
-								color: #FFF;
-								font-size: 3rem;
-								opacity: 0.5;
-							}
-							#modal-container .modal-background:hover::before {
-								cursor: pointer;
-							}
-							#modal-container .modal {
-								width: 80%;
-								max-width: 639px;
-								position: relative;
-								margin: auto;
-							}
-							#modal-container .modal img {
-								width: 100%;
-							}
-							#modal-container.one {
-								transform: scaleY(0.01) scaleX(0);
-								animation: unfoldIn 1s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
-							}
-							#modal-container.one .modal-background .modal {
-								transform: scale(0);
-								animation: zoomIn 0.5s 0.8s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
-							}
-							#modal-container.one.out {
-								transform: scale(1);
-								animation: unfoldOut 1s 0.3s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
-							}
-							#modal-container.one.out .modal-background .modal {
-								animation: zoomOut .5s cubic-bezier(0.165, 0.840, 0.440, 1.000) forwards;
-							}
-							@keyframes unfoldIn {
-								0% {
-									transform:scaleY(.005) scaleX(0);
-								}
-								50% {
-									transform:scaleY(.005) scaleX(1);
-								}
-								100% {
-									transform:scaleY(1) scaleX(1);
-								}
-							}
-							@keyframes zoomIn {
-								0% {
-									transform:scale(0);
-								}
-								100% {
-									transform:scale(1);
-								}
-							}
-							@keyframes unfoldOut {
-								0% {
-									transform:scaleY(1) scaleX(1);
-								}
-								50% {
-									transform:scaleY(.005) scaleX(1);
-								}
-								100% {
-									transform:scaleY(.005) scaleX(0);
-								}
-							}
-							@keyframes zoomOut {
-								0% {
-									transform:scale(1);
-								}
-								100% {
-									transform:scale(0);
-								}
-							}
-						</style>
-						<div id="modal-background" class="modal-background">
-							<div class="modal atm-input__search-mobile">
-								<form action="/buscador" method="GET" class="search-mobile__form" autocomplete="off">
-									<input name="buscando" type="search" placeholder="Buscar en Líbero ..." results=0 class="search-mobile__input">
-									<button type="submit" class="search-mobile__submit">
-										<i class="fa fa-search" aria-hidden="true"></i>
-									</button>
-								</form>
-							</div>
-						</div>`;
-	return htmltemplate;
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', fn);
+    } else {
+        document.attachEvent('onreadystatechange', function () {
+            if (document.readyState != 'loading')
+                fn();
+        });
+    }
 }
-function closeModal(e) {
-	let modalBackground = document.getElementById("modal-background"),
-		modalContainer = document.getElementById("modal-container");
-	if ((e.type === 'click' && e.target === modalBackground) || 
-		(e.type === 'keyup' && (e.keycode === 27 || e.which ===27))){
-			//tagBody.classList.remove("modal-active");
-			modalContainer.classList.add("out");
-			setTimeout(endAnimation, 3000);
+
+function domReadyToGo() {
+
+    function callObj(obj) {
+        return obj.run();
+    }
+    
+    window.onload = function (e) {
+        //Este método se llama cuando se termina de cargar toda la pagina
+    }
+
+
+	/********** AnimationSearchJs **********/
+	if(document.getElementById('js-animation-search')) {
+	    var btnSearch = document.getElementById('js-animation-search');
+
+	    function createElementModal() {
+	        let htmlTemplate = `<style>
+	                        @media (min-width: 1000px) {
+	                            html.animation-search {
+	                                overflow: hidden;
+	                            }
+	                            body.animation-search {
+	                                padding-right: 16px;
+	                            } 
+	                            #modal-container {
+	                                position: fixed;
+	                                height: 100vh;
+	                                width: 100%;
+	                                top: 0;
+	                                left: 0;
+	                                right: 0;
+	                                bottom: 0;
+	                                transform: scale(0);
+	                                z-index: 3;
+	                                background: rgba(55, 95, 210, 0.9);
+	                                overflow-x: hidden;
+	                                overflow-y: auto;
+	                            }
+	                            #modal-container .modal-background {
+	                                text-align: center;
+	                                height: 100%;
+	                                width: 100%;
+	                                display: flex;
+	                                padding: 15px 0;
+	                            }
+	                            #modal-container .modal-background::before {
+	                                content: "X";
+	                                position: absolute;
+	                                top: 1.5rem;
+	                                right: 1.5rem;
+	                                color: #FFF;
+	                                font-size: 3rem;
+	                                opacity: 0.5;
+	                                font-family: 'Helvetica', 'Arial', 'GothamMedium';
+	                                font-weight: 500;
+	                            }
+	                            #modal-container .modal-background:hover::before {
+	                                cursor: pointer;
+	                            }
+	                            #modal-container .modal {
+	                                width: 80%;
+	                                max-width: 639px;
+	                                position: relative;
+	                                margin: auto;
+	                            }
+	                            #modal-container .modal img {
+	                                width: 100%;
+	                            }
+	                            #modal-container.one {
+	                                transform: scaleY(0.01) scaleX(0);
+	                                animation: unfoldIn 1s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
+	                            }
+	                            #modal-container.one .modal-background .modal {
+	                                transform: scale(0);
+	                                animation: zoomIn 0.5s 0.8s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
+	                            }
+	                            #modal-container.one.out {
+	                                transform: scale(1);
+	                                animation: unfoldOut 1s 0.3s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
+	                            }
+	                            #modal-container.one.out .modal-background .modal {
+	                                animation: zoomOut .5s cubic-bezier(0.165, 0.840, 0.440, 1.000) forwards;
+	                            }
+	                            @keyframes unfoldIn {
+	                                0% {
+	                                    transform:scaleY(.005) scaleX(0);
+	                                }
+	                                50% {
+	                                    transform:scaleY(.005) scaleX(1);
+	                                }
+	                                100% {
+	                                    transform:scaleY(1) scaleX(1);
+	                                }
+	                            }
+	                            @keyframes zoomIn {
+	                                0% {
+	                                    transform:scale(0);
+	                                }
+	                                100% {
+	                                    transform:scale(1);
+	                                }
+	                            }
+	                            @keyframes unfoldOut {
+	                                0% {
+	                                    transform:scaleY(1) scaleX(1);
+	                                }
+	                                50% {
+	                                    transform:scaleY(.005) scaleX(1);
+	                                }
+	                                100% {
+	                                    transform:scaleY(.005) scaleX(0);
+	                                }
+	                            }
+	                            @keyframes zoomOut {
+	                                0% {
+	                                    transform:scale(1);
+	                                }
+	                                100% {
+	                                    transform:scale(0);
+	                                }
+	                            }
+	                        }
+	                        </style>
+	                        <div id="modal-background" class="modal-background">
+	                            <div class="modal atm-input__search-mobile">
+	                                <form action="/buscador" method="GET" class="search-mobile__form" autocomplete="off">
+	                                    <input name="buscando" type="search" placeholder="Buscar en Líbero ..." results=0 class="search-mobile__input">
+	                                    <button type="submit" class="search-mobile__submit">
+	                                        <i class="fa fa-search" aria-hidden="true"></i>
+	                                    </button>
+	                                </form>
+	                            </div>
+	                        </div>`;
+
+	        return htmlTemplate;
+	    }
+	    function closeModal(e) {
+	        let modalBackground = document.getElementById("modal-background"),
+	            modalContainer = document.getElementById("modal-container");
+	        if ((e.type === 'click' && e.target === modalBackground) || 
+	            (e.type === 'keyup' && (e.keycode === 27 || e.which ===27))){
+	                //tagBody.classList.remove("modal-active");
+	                modalContainer.classList.add("out");
+	                setTimeout(endAnimation, 1500);
+	        }
+	        document.body.classList.remove('animation-search');
+	        document.documentElement.classList.remove('animation-search');            
+	    }
+	    function endAnimation() {
+	        let modalContainer = document.getElementById("modal-container");
+	        modalContainer.remove();
+	    }
+	    function animation(e){
+	        let element = e.target;
+	        if(element.nodeName == "I") {
+	            element = e.target.parentElement;
+	        }
+
+	        let modalContainer = document.createElement('div');
+	        modalContainer.setAttribute('id', 'modal-container');
+	        modalContainer.setAttribute('class', 'one');
+	        modalContainer.innerHTML = createElementModal();
+
+	        document.body.classList.add('animation-search');
+	        document.documentElement.classList.add('animation-search');
+
+
+	        document.body.appendChild(modalContainer);
+
+	        modalContainer.addEventListener('click', closeModal);
+	    }
+	    btnSearch.addEventListener('click', animation);
 	}
-}
-function endAnimation() {
-	let modalContainer = document.getElementById("modal-container");
-	modalContainer.remove();
-}
-function animation(e){
-	let element = e.target;
-	if(element.nodeName == "I") {
-		element = e.target.parentElement;
+	/********** End - AnimationSearchJs **********/
+
+	/************* HeaderStickyPartialJs *************/
+	if (window.matchMedia("(min-width: 1000px)").matches) {
+		if(document.querySelector('.tem-Wrapper')) {
+			var wrapper = document.querySelector('.tem-Wrapper');
+			var temWrapperHeader = document.querySelector('.tem_Wrapper-header');
+			var headerSticky = document.getElementById('tem_Header-sticky');
+			var headerStickyHeight = temWrapperHeader.getBoundingClientRect().height; 
+			var headerHeight = temWrapperHeader.getBoundingClientRect().height;
+	    	var sticky = (wrapper.getBoundingClientRect().top)*(-1);
+			var sum = sticky + headerHeight;
+			let tagHtml = document.querySelector('html');
+	        let tagBody = document.querySelector('body');
+
+			function myFunction() {
+			//console.log("scroll",window.pageYOffset,"distancia",sum);
+			let temHeader = document.querySelector('.tem_Header'),
+				temHeaderHeight = temHeader.getBoundingClientRect().height;
+				//console.log(temWrapperHeader);
+				if (window.scrollY >= sum || window.pageYOffset >= sum) {
+					wrapper.classList.add("is-active-sticky");
+					tagBody.classList.add("is-active-sticky");
+					tagHtml.classList.add("is-active-sticky");
+					//headerSticky.style.height = headerHeight + 'px';
+					//headerSticky.style.marginTop = headerHeight + 'px';
+					headerSticky.style.height = temHeaderHeight + 'px';
+				} else {
+					wrapper.classList.remove("is-active-sticky");
+					tagBody.classList.remove("is-active-sticky");
+					tagHtml.classList.remove("is-active-sticky");
+					//headerSticky.setAttribute("style", "");
+					headerSticky.removeAttribute("style");
+				}
+			}
+			window.onscroll = function() {myFunction()};
+
+		}
 	}
+	/********** End - HeaderStickyPartialJs **********/
 
-	let modalContainer = document.createElement('div');
-	modalContainer.setAttribute('id', 'modal-container');
-	modalContainer.setAttribute('class', 'one');
-	modalContainer.innerHTML = createElementModal();
-
-	document.body.appendChild(modalContainer);
-
-	modalContainer.addEventListener('click', closeModal);
 }
-
-btnSearch.addEventListener('click', animation);
+setTimeout(function () {
+    ready(domReadyToGo);
+}, 2000);
